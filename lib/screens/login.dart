61175968738c9f70706fb570465d909eae9d1eac
abd33 <alaziizz67@gmail.com>
2025-08-26 19:40:14 +0300

    change login custome update customer info page automating bar code scanning

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -12,11 +12,10 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login Screen')),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.all(40.0),
+            padding: const EdgeInsets.only(top: 70, left: 50, right: 50),
             child: ConstrainedBox(
               constraints: BoxConstraints(
                 minHeight:
@@ -31,31 +30,49 @@ class _LoginScreenState extends State<LoginScreen> {
                   // const Spacer(),
                   const Text(
                     'Welcome!',
-                    style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 21, 88, 136),
+                      fontSize: 34,
+                      fontWeight: FontWeight.w900,
+                    ),
                   ),
                   const Text(
-                    'Please login to continue',
-                    style: TextStyle(fontSize: 18),
+                    'to your daily compnainon',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color.fromARGB(255, 15, 90, 143),
+                    ),
                   ),
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 40),
                   TextField(
                     textAlign: TextAlign.center,
+                    style: TextStyle(color: Color.fromARGB(255, 12, 71, 114)),
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(50)),
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 12, 71, 114),
+                        ),
                         // borderSide: BorderSide(color: Colors.black, width: 2),
                       ),
                       labelText: 'Username',
-                      suffixIcon: Icon(Icons.person),
+                      suffixIcon: Icon(
+                        Icons.person,
+                        color: Color.fromARGB(255, 12, 71, 114),
+                      ),
                     ),
                   ),
                   const SizedBox(height: 10),
                   TextField(
                     obscureText: _obscureText,
                     textAlign: TextAlign.center,
+                    style: TextStyle(color: Color.fromARGB(255, 12, 71, 114)),
                     decoration: InputDecoration(
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(50)),
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 0, 34, 58),
+                        ),
                         // borderSide: BorderSide(color: Colors.black, width: 2),
                       ),
                       labelText: 'Password',
@@ -64,6 +81,7 @@ class _LoginScreenState extends State<LoginScreen> {
                           _obscureText
                               ? Icons.visibility_off
                               : Icons.visibility,
+                          color: Color.fromARGB(255, 12, 71, 114),
                         ),
                         onPressed: () {
                           _obscureText = !_obscureText;
@@ -84,7 +102,12 @@ class _LoginScreenState extends State<LoginScreen> {
                         Navigator.pushNamed(context, '/salesScreen');
                       },
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.black, // Button color
+                        backgroundColor: Color.fromARGB(
+                          255,
+                          12,
+                          71,
+                          114,
+                        ), // Button color
                         foregroundColor: Colors.white, // Text color
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(50),
@@ -97,7 +120,43 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
 
-                  const SizedBox(height: 20),
+                  TextButton(
+                    onPressed: () {
+                      // Handle forgot password logic here
+                      Navigator.pushNamed(context, '/forgot-password');
+                    },
+                    style: TextButton.styleFrom(
+                      foregroundColor: Color.fromARGB(255, 12, 71, 114),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10,
+                      ), // Padding
+                    ),
+
+                    child: RichText(
+                      text: TextSpan(
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 12, 71, 114),
+                          fontSize: 16,
+                          // fontWeight: FontWeight.bold,
+                        ),
+                        children: const [
+                          TextSpan(text: 'Forgot Password?'),
+                          TextSpan(
+                            text: ' Reset',
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 12, 71, 114),
+                              fontStyle: FontStyle.italic,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+
                   SizedBox(
                     width: double.infinity,
                     height: 50,
@@ -107,9 +166,17 @@ class _LoginScreenState extends State<LoginScreen> {
                         Navigator.pushNamed(context, '/register');
                       },
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.black, // Button color
+                        backgroundColor: Color.fromARGB(
+                          255,
+                          12,
+                          71,
+                          114,
+                        ), // Button color
                         foregroundColor: Colors.white, // Text color
                         padding: const EdgeInsets.symmetric(), // Padding
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
                       ),
                       child: const Text(
                         'Sign Up',
@@ -122,41 +189,6 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                   const SizedBox(height: 20),
-                  TextButton(
-                    onPressed: () {
-                      // Handle forgot password logic here
-                      Navigator.pushNamed(context, '/forgot-password');
-                    },
-                    style: TextButton.styleFrom(
-                      foregroundColor: Colors.black,
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 20,
-                        vertical: 10,
-                      ), // Padding
-                    ),
-
-                    child: RichText(
-                      text: TextSpan(
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                          // fontWeight: FontWeight.bold,
-                        ),
-                        children: const [
-                          TextSpan(text: 'Forgot Password?'),
-                          TextSpan(
-                            text: ' Reset',
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 40),
 
                   //  const Spacer(),
                 ],
