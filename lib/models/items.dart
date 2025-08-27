e562c6ea930ce443b0d9a6c8279af6590228da96
abd33 <alaziizz67@gmail.com>
2025-08-27 20:07:23 +0300

    added barcode object(may be all items will  have barcode, not required)

--- lib/models/items.dart
+++ lib/models/items.dart
@@ -2,6 +2,12 @@ class Item {
   final String id;
   final String description;
   final String uom;
+  final double barcode;
 
-  Item({required this.id, required this.description, required this.uom});
+  Item({
+    required this.id,
+    required this.description,
+    required this.uom,
+    this.barcode = 0,
+  });
 }
