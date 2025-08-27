a915918ea2f10089eb4e3b3070649bb99af33599
abd33 <alaziizz67@gmail.com>
2025-08-27 20:13:04 +0300

    the dropdown is now looks like the design

--- lib/widgets/salesEntry.dart/salesorder.dart/customerdropdown.dart
+++ lib/widgets/salesEntry.dart/salesorder.dart/customerdropdown.dart
@@ -4,12 +4,11 @@ import 'package:savvy_stock/models/customer.dart';
 class CustomerDropdown extends StatefulWidget {
   final Customer? value;
   final ValueChanged<Customer?> onChanged;
-  final String hintText;
+
   const CustomerDropdown({
     super.key,
     required this.value,
     required this.onChanged,
-    this.hintText = 'Select Customer',
   });
 
   @override
@@ -17,15 +16,27 @@ class CustomerDropdown extends StatefulWidget {
 }
 
 class _CustomerDropdownState extends State<CustomerDropdown> {
-  final OverlayPortalController _overlayPortalController =
-      OverlayPortalController();
-  final _link = LayerLink();
+  bool _isExpanded = false;
   String _selectedCustomer = '';
 
   @override
   void initState() {
     super.initState();
-    _selectedCustomer = widget.value?.name ?? '--Selecte Customer--';
+    _selectedCustomer = widget.value?.name ?? '';
+  }
+
+  void _toggleExpand() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+    });
+  }
+
+  void _selectCustomer(Customer customer) {
+    setState(() {
+      _selectedCustomer = customer.name;
+      _isExpanded = false;
+    });
+    widget.onChanged(customer);
   }
 
   // Mock customer data
@@ -65,172 +76,180 @@ class _CustomerDropdownState extends State<CustomerDropdown> {
       phone: '555-7890',
       country: 'Japan',
     ),
+    Customer(
+      id: '6',
+      name: 'Acme Corp',
+      tin: '456123789',
+      phone: '555-9012',
+      country: 'UK',
+    ),
+    Customer(
+      id: '7',
+      name: 'Global Enterprises',
+      tin: '789123456',
+      phone: '555-3456',
+      country: 'Germany',
+    ),
+    Customer(
+      id: '8',
+      name: 'Tech Solutions Ltd',
+      tin: '321654987',
+      phone: '555-7890',
+      country: 'Japan',
+    ),
   ];
-
-  Widget _buildTableCell(String text) {
-    return GestureDetector(
-      onTap: () {
-        final customer = customers.firstWhere(
-          (c) =>
-              c.name == text ||
-              c.phone == text ||
-              c.tin == text ||
-              c.country == text,
-        );
-        setState(() {
-          _selectedCustomer = customer.name;
-        });
-        widget.onChanged(customer);
-        _overlayPortalController.hide();
-      },
-      child: Padding(padding: const EdgeInsets.all(8.0), child: Text(text)),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
-    return CompositedTransformTarget(
-      link: _link,
-      child: OverlayPortal(
-        controller: _overlayPortalController,
-        overlayChildBuilder: (context) {
-          return CompositedTransformFollower(
-            link: _link,
-            targetAnchor: Alignment.bottomLeft,
-            offset: const Offset(0, -30),
-            child: Align(
-              alignment: AlignmentDirectional.topStart,
-              child: Material(
-                clipBehavior: Clip.antiAliasWithSaveLayer,
-                elevation: 4.0,
-                child: ConstrainedBox(
-                  constraints: const BoxConstraints(maxHeight: 200),
-                  child: SingleChildScrollView(
-                    child: Container(
-                      padding: const EdgeInsets.only(top: 20.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: const BorderRadius.only(
-                          bottomLeft: Radius.circular(50.0),
-                          bottomRight: Radius.circular(50.0),
-                        ),
-                        border: Border(
-                          left: BorderSide(color: Colors.black),
-                          right: BorderSide(color: Colors.black),
-                          bottom: BorderSide(color: Colors.black),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.2),
-                            spreadRadius: 1,
-                            blurRadius: 4,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      child: Table(
-                        border: TableBorder(
-                          top: BorderSide.none,
-                          left: BorderSide.none,
-                          right: BorderSide.none,
-                          horizontalInside: BorderSide(
-                            color: Colors.grey.shade300,
-                          ),
-                          verticalInside: BorderSide(
-                            color: Colors.grey.shade300,
-                          ),
-                        ),
-                        columnWidths: const {
-                          0: FixedColumnWidth(150),
-                          1: FixedColumnWidth(100),
-                          2: FixedColumnWidth(120),
-                        },
-                        textBaseline: TextBaseline.alphabetic,
-                        children: [
-                          // Table header
-                          TableRow(
-                            decoration: BoxDecoration(color: Colors.amber),
-                            children: const [
-                              Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Name',
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Phone',
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.all(8.0),
-                                child: Text(
-                                  'TIN',
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                            ],
-                          ),
-                          // Table rows
-                          ...customers
-                              .map(
-                                (customer) => TableRow(
-                                  decoration: BoxDecoration(
-                                    color: _selectedCustomer == customer.name
-                                        ? Colors.blue[50]
-                                        : Colors.white,
-                                  ),
-                                  children: [
-                                    _buildTableCell(customer.name),
-                                    _buildTableCell(customer.phone),
-                                    _buildTableCell(customer.tin),
-                                  ],
-                                ),
-                              )
-                              .toList(),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          );
-        },
-        child: GestureDetector(
-          onTap: () {
-            if (_overlayPortalController.isShowing) {
-              _overlayPortalController.hide();
-            } else {
-              _overlayPortalController.show();
-            }
-          },
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 14),
+    return Container(
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(50.0)),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          // Header with selected customer and toggle button
+          Container(
             decoration: BoxDecoration(
-              border: Border.all(color: Color.fromARGB(255, 21, 88, 136)),
-              borderRadius: BorderRadius.circular(30.0),
+              color: Color.fromARGB(220, 228, 228, 228),
+              borderRadius: BorderRadius.only(
+                topLeft: const Radius.circular(50.0),
+                topRight: const Radius.circular(50.0),
+                bottomLeft: _isExpanded
+                    ? Radius.zero
+                    : const Radius.circular(50.0),
+                bottomRight: _isExpanded
+                    ? Radius.zero
+                    : const Radius.circular(50.0),
+              ),
+              border: Border.all(color: Colors.black, width: 1.0),
             ),
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  _selectedCustomer,
-                  style: TextStyle(
-                    color: _selectedCustomer.isNotEmpty
-                        ? Colors.black
-                        : Colors.grey,
+                  _selectedCustomer.isEmpty
+                      ? '--Select Customer--'
+                      : _selectedCustomer,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
                   ),
                 ),
-                Icon(Icons.arrow_drop_down, color: Colors.grey),
+                IconButton(
+                  icon: Icon(
+                    _isExpanded ? Icons.arrow_drop_up : Icons.arrow_drop_down,
+                  ),
+                  onPressed: _toggleExpand,
+                ),
               ],
             ),
           ),
-        ),
+
+          // Expandable content
+          if (_isExpanded)
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: const BoxDecoration(
+                color: Color(0xFFFDD400),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10.0),
+                  bottomRight: Radius.circular(10.0),
+                ),
+                border: Border(
+                  left: BorderSide(color: Colors.black, width: 1.0),
+                  right: BorderSide(color: Colors.black, width: 1.0),
+                  bottom: BorderSide(color: Colors.black, width: 1.0),
+                ),
+              ),
+              height: 200,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    // Table header
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0,
+                        vertical: 8.0,
+                      ),
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Colors.black, width: 1.0),
+                        ),
+                      ),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Name',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          Text(
+                            'Phone',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                          Text(
+                            'TIN',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    // Customer list
+                    ...customers.map((customer) {
+                      final bool isSelected =
+                          _selectedCustomer == customer.name;
+                      return InkWell(
+                        onTap: () => _selectCustomer(customer),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: isSelected
+                                ? const Color(0xFF1C5380)
+                                : Colors.transparent,
+                            border: const Border(
+                              bottom: BorderSide(
+                                color: Colors.black,
+                                width: 1.0,
+                              ),
+                            ),
+                          ),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 16.0,
+                            vertical: 12.0,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              _buildTableCell(customer.name),
+                              _buildTableCell(customer.phone),
+                              _buildTableCell(customer.tin),
+                            ],
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ],
+                ),
+              ),
+            ),
+        ],
       ),
+    );
+  }
+
+  Widget _buildTableCell(String text) {
+    return Text(
+      text,
+      style: const TextStyle(color: Colors.black, fontSize: 14.0),
     );
   }
 }
