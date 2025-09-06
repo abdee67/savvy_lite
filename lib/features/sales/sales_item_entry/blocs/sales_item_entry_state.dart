5fc948401af9b9f15d98513209a45777a36b9dab
abd33 <alaziizz67@gmail.com>
2025-09-06 11:53:57 +0300

    when confirmed items move into selecteditem it will compare by item id and store id and there is a fallback ofc

--- lib/features/sales/sales_item_entry/blocs/sales_item_entry_state.dart
+++ lib/features/sales/sales_item_entry/blocs/sales_item_entry_state.dart
@@ -1,12 +1,14 @@
 import 'package:equatable/equatable.dart';
 import 'package:savvy_stock/features/sales/sales_item_entry/models/item_in_store.dart';
 import 'package:savvy_stock/features/sales/sales_item_entry/models/confirmed_item.dart';
+import 'package:savvy_stock/features/sales/sales_item_entry/models/items.dart';
 import 'package:savvy_stock/features/sales/sales_item_entry/models/selected_item.dart';
 
 enum ItemEntryStatus { initial, loading, success, failure }
 
 class ItemEntryState extends Equatable {
   final ItemEntryStatus status;
+  final List<Item> uniqueItems;
   final List<ItemInStore> itemsInStores;
   final List<SelectedItem> selectedItems;
   final List<ConfirmedItem> confirmedItems;
@@ -20,6 +22,7 @@ class ItemEntryState extends Equatable {
     this.itemsInStores = const [],
     this.selectedItems = const [],
     this.confirmedItems = const [],
+    this.uniqueItems = const [],
     this.useBarcode = false,
     this.errorMessage,
     this.totalAmount = 0,
@@ -31,6 +34,7 @@ class ItemEntryState extends Equatable {
     List<ItemInStore>? itemsInStores,
     List<SelectedItem>? selectedItems,
     List<ConfirmedItem>? confirmedItems,
+    List<Item>? uniqueItems,
     bool? useBarcode,
     String? errorMessage,
     double? totalAmount,
@@ -41,6 +45,7 @@ class ItemEntryState extends Equatable {
       itemsInStores: itemsInStores ?? this.itemsInStores,
       selectedItems: selectedItems ?? this.selectedItems,
       confirmedItems: confirmedItems ?? this.confirmedItems,
+      uniqueItems: uniqueItems ?? this.uniqueItems,
       useBarcode: useBarcode ?? this.useBarcode,
       errorMessage: errorMessage ?? this.errorMessage,
       totalAmount: totalAmount ?? this.totalAmount,
@@ -52,6 +57,7 @@ class ItemEntryState extends Equatable {
   bool get hasValidItems => selectedItems.any((item) => item.isValid);
 
   bool get hasConfirmedItems => confirmedItems.isNotEmpty;
+  bool get hasItems => uniqueItems.isNotEmpty;
   bool get hasSelectedConfirmedItems => selectedConfirmedItemIndices.isNotEmpty;
 
   @override
@@ -60,6 +66,7 @@ class ItemEntryState extends Equatable {
     itemsInStores,
     selectedItems,
     confirmedItems,
+    uniqueItems,
     useBarcode,
     errorMessage,
     totalAmount,
--- lib/features/sales/sales_item_entry/models/confirmed_item.dart
+++ lib/features/sales/sales_item_entry/models/confirmed_item.dart
@@ -1,16 +1,20 @@
 import 'package:equatable/equatable.dart';
 
 class ConfirmedItem extends Equatable {
+  final String itemId;
   final String itemName;
   final double quantity;
   final double totalPrice;
+  final String? storeId;
 
   const ConfirmedItem({
+    required this.itemId,
     required this.itemName,
     required this.quantity,
     required this.totalPrice,
+    this.storeId,
   });
 
   @override
-  List<Object?> get props => [itemName, quantity, totalPrice];
+  List<Object?> get props => [itemId, itemName, quantity, totalPrice, storeId];
 }
