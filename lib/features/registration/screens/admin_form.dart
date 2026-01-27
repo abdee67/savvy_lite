8d7e91fb872dcf57a5c4c6f7aa58cbf6f0026a85
abd33 <alaziizz67@gmail.com>
2026-01-27 13:35:17 +0300

    add slight diff for indicator

--- lib/features/registration/screens/admin_form.dart
+++ lib/features/registration/screens/admin_form.dart
@@ -174,7 +174,7 @@ class _AdminFormScreenState extends State<AdminFormScreen> {
               // 🔹 Scrollable Form
               SingleChildScrollView(
                 padding: EdgeInsets.symmetric(
-                  horizontal: isTablet ? 80 : 20,
+                  horizontal: isTablet ? 30 : 18,
                   vertical: isTablet ? 100 : 40,
                 ),
                 child: Column(
