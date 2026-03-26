d796165a18175584650c9fbff81419f14e357bc4
abd33 <alaziizz67@gmail.com>
2026-03-26 15:39:07 +0300

    feat(stock): extend BaseRepository and integrate captureSync in ItemTransactionRepository

--- lib/features/stock/item_transactions/repo/item_transaction_repo.dart
+++ lib/features/stock/item_transactions/repo/item_transaction_repo.dart
@@ -1,7 +1,7 @@
-// repositories/item_transaction_repository.dart
 import 'dart:developer' as developer;
 
 import 'package:flutter/foundation.dart';
+import 'package:savvy_stock/core/repositories/base_repo.dart';
 import 'package:savvy_stock/features/next_number/repo/next_number_repo.dart';
 import 'package:savvy_stock/features/purchase/purchase_entry/models/purchase_order_receiver_model.dart';
 import 'package:savvy_stock/features/sales/sales_order/detail/model/sales_order_detail.dart';
@@ -23,7 +23,8 @@ import 'package:savvy_stock/features/stock/item_transactions/model/item_transact
 import 'package:savvy_stock/features/stock/location_entry/repo/location_master_repository.dart';
 import 'package:sqflite/sqflite.dart';
 
-class ItemTransactionRepository {
+class ItemTransactionRepository extends BaseRepository {
+  @override
   final LocalDatabaseService databaseService;
   final AuthBloc authBloc;
   final SystemConstantBloc systemConstantBloc;
@@ -260,6 +261,13 @@ class ItemTransactionRepository {
     );
 
     await db.insert('item_transactions', transaction.toMap());
+    captureSync(
+      tableName: 'item_transactions',
+      entityMap: transaction.toMap(),
+      entityId: transaction.id.toString(),
+      operation: 'INSERT',
+      company: companyId.toString(),
+    );
 
     // Update item branch quantity
     // final updatedIb = ib.copyWith(quantityAvailable: qtyAvInStore);(Not necessary fpr sales order it will update there not here)
@@ -393,11 +401,16 @@ class ItemTransactionRepository {
     );
 
     await db.insert('item_transactions', transaction.toMap());
+    captureSync(
+      tableName: 'item_transactions',
+      entityMap: transaction.toMap(),
+      entityId: transaction.id.toString(),
+      operation: 'INSERT',
+      company: companyId.toString(),
+    );
 
     // Update item location quantity
-    final updatedLoc = loc.copyWith(
-      quantityOnHand: (loc.quantityOnHand ?? 0.0) + qty,
-    );
+    loc.copyWith(quantityOnHand: (loc.quantityOnHand ?? 0.0) + qty);
     // await itemLocationsRepository.updateItemLocation(updatedLoc);//Not necessary fpr sales order it will update there not here
 
     // Note: Item branch quantity update removed - now handled by cascading save in location repository
@@ -570,17 +583,20 @@ class ItemTransactionRepository {
       developer.log('DEBUG: Inserting transaction');
     }
     await db.insert('item_transactions', transaction.toMap());
+    captureSync(
+      tableName: 'item_transactions',
+      entityMap: transaction.toMap(),
+      entityId: transaction.id.toString(),
+      operation: 'INSERT',
+      company: companyId.toString(),
+    );
 
     // Update lot quantity
-    final updatedLm = lm.copyWith(
-      quantityAvailable: (lm.quantityAvailable ?? 0.0) + qty,
-    );
+    lm.copyWith(quantityAvailable: (lm.quantityAvailable ?? 0.0) + qty);
     //await lotMasterRepository.updateLotMaster(updatedLm);//Not necessary for sales order it will update there not here
 
     // Update item branch quantity
-    final updatedIb = ib.copyWith(
-      quantityAvailable: qtyAvInStore + (factorP * qty),
-    );
+    ib.copyWith(quantityAvailable: qtyAvInStore + (factorP * qty));
     //await itemInBranchRepository.update(updatedIb);//Not necessary for sales order it will update there not here
   }
 
@@ -733,6 +749,13 @@ class ItemTransactionRepository {
           explicitCode: transactionTypeCode,
         );
         batch.insert('item_transactions', enriched.toMap());
+        captureSync(
+          tableName: 'item_transactions',
+          entityMap: enriched.toMap(),
+          entityId: enriched.id.toString(),
+          operation: 'INSERT',
+          company: companyId.toString(),
+        );
       }
 
       await batch.commit();
@@ -2056,7 +2079,17 @@ class ItemTransactionRepository {
 
   Future<int> createTransaction(ItemTransactionModel transaction) async {
     final db = await databaseService.database;
-    return await db.insert('item_transactions', transaction.toMap());
+    final id = await db.insert('item_transactions', transaction.toMap());
+
+    captureSync(
+      tableName: 'item_transactions',
+      entityMap: transaction.toMap(),
+      entityId: id.toString(),
+      operation: 'INSERT',
+      company: authBloc.state.companyId.toString(),
+    );
+
+    return id;
   }
 
   Future<void> updateTransaction(ItemTransactionModel transaction) async {
@@ -2067,6 +2100,13 @@ class ItemTransactionRepository {
       where: 'id = ? AND company = ?',
       whereArgs: [transaction.id, authBloc.state.companyId],
     );
+    captureSync(
+      tableName: 'item_transactions',
+      entityMap: transaction.toMap(),
+      entityId: transaction.id.toString(),
+      operation: 'UPDATE',
+      company: authBloc.state.companyId.toString(),
+    );
   }
 
   Future<void> deleteTransaction(int id) async {
@@ -2075,6 +2115,13 @@ class ItemTransactionRepository {
       'item_transactions',
       where: 'id = ? AND company = ?',
       whereArgs: [id, authBloc.state.companyId],
+    );
+    captureSync(
+      tableName: 'item_transactions',
+      entityMap: {'id': id, 'company': authBloc.state.companyId},
+      entityId: id.toString(),
+      operation: 'DELETE',
+      company: authBloc.state.companyId.toString(),
     );
   }
 
@@ -2090,6 +2137,13 @@ class ItemTransactionRepository {
           'item_transactions',
           where: 'id = ? AND company = ?',
           whereArgs: [transaction.id, authBloc.state.companyId],
+        );
+        captureSync(
+          tableName: 'item_transactions',
+          entityMap: {'id': transaction.id, 'company': authBloc.state.companyId},
+          entityId: transaction.id.toString(),
+          operation: 'DELETE',
+          company: authBloc.state.companyId.toString(),
         );
       }
     }
